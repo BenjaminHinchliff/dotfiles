7a4c74b4a8660b356f7563bc27854e910711a5b2
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2023-03-25 14:00:17 -0700

    update configs and wallpaper and change firefox config

--- .config/nvimuser/init.lua
+++ .config/nvimuser/init.lua
@@ -1,25 +1,84 @@
 return {
-  colorscheme = "catppuccin",
+  colorscheme = "gruvbox",
+  highlights = {
+    gruvbox = {
+      StatusLine = { fg = "#ebdbb2", bg = "#504945" },
+    },
+  },
   options = {
     o = {},
     g = {
       vimtex_view_general_viewer = "okular"
     },
   },
+  lsp = {
+    ["server-settings"] = {
+      rust_analyzer = {
+        settings = {
+          ["rust-analyzer"] = {
+            checkOnSave = {
+              allTargets = true,
+            },
+          },
+        },
+      },
+    },
+  },
   plugins = {
-    init = {
-      {
-        "catppuccin/nvim",
-        as = "catppuccin",
-        config = function()
-          require("catppuccin").setup {
-            flavour = "mocha" -- mocha, macchiato, frappe, latte
-          }
-        end
-      },
-      {
-        "lervag/vimtex"
-      },
+    {
+      "ellisonleao/gruvbox.nvim",
+      -- name = "catppuccin",
+      -- config = function()
+      --   require("catppuccin").setup {
+      --     flavour = "mocha" -- mocha, macchiato, frappe, latte
+      --   }
+      -- end
+    },
+    {
+      "lervag/vimtex"
+    },
+    {
+      "elkowar/yuck.vim"
+    },
+    {
+
+    },
+    {
+      "nvim-treesitter/nvim-treesitter",
+      dependencies = { "HiPhish/nvim-ts-rainbow2" },
+      opts = function(_, opts)
+        opts.rainbow = {
+          enable = true,
+          disable = { "html" },
+          extended_mode = false,
+          max_file_lines = nil,
+        }
+      end
+    },
+    {
+      "goolord/alpha-nvim",
+      opts = function(_, opts)
+        opts.section.header.val = {
+          "                            ,▄,  ▄▓▄  ,▄",
+          "                        █▄µ ███▓▄███▄▓███ ╓▄█",
+          "                   ╒▄▄µ▄█████████████████████▄╓▄▄▄",
+          "    ,           ,  ▐█████████████████████████████▌  ,        ╓▄▌",
+          " ▄─ ▐██▄       ▐█████████████████████████████████████H     ▄███H  ▄█",
+          "███  ████   ,▄▄╓█████████████████████████████████████╓▄▄  ▐████┐,▄██",
+          "████▄████▌  ▐███████████████████████████████████████████┘ ▐████████▀",
+          "`▀███████¬,╓▄██████████████████████████████████████████▌▄╓,▀█████▀",
+          "   ╙▀███─ ▀███████████████████▀▀████████▀▀████████████████▀J██▀╙",
+          "      ▀██▓▄██████████████████▓▓  ██████▓▓  ██████████████████└",
+          "        ▐████████████████████    ██████    █████████████████▄",
+          "       ▄██████████████████████▄▄████████▄▄█████████████▀██████H",
+          "       ²▀███ ╙▓▄\"╙▀▀██████████████▄  ▄███████████▓▀▀╙  ▓▀ ███┘",
+          "         `▀██µ ▀▄      `╙╙╙▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀╙\"╙`       ▓┘ ▓█▀",
+          "            ▀█▄ `N                                   Å  ▄█╜",
+          "              ▀█                                       ▄▀",
+          "                ╨                                      ╙",
+        }
+        opts.section.header.opts.hl = "GruvboxOrange"
+      end,
     },
   },
 }
--- .zshrc
+++ .zshrc
@@ -24,13 +24,58 @@ autoload -Uz compinit
 compinit
 # End of lines added by compinstall
 
+setopt correct
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+
 # Add nvm
 source /usr/share/nvm/init-nvm.sh
 
-export PATH="/home/benja/.cache/yay/distrobox/pkg/distrobox/usr/bin:/home/benja/.local/bin/:$PATH"
+export PATH="$HOME/.local/bin/:$HOME/bin:$PATH"
+export PAGER="nvimpager"
+
 alias config='/usr/bin/git --git-dir=/home/benja/.dotfiles --work-tree=/home/benja'
 alias ls="exa --icons"
 alias locate="plocate"
-alias hx="helix"
 alias ssh="kitty +kitten ssh"
 alias icat="kitty +kitten icat --align=left"
+alias nnn="nnn -ade"
+alias make="make -j$(nproc)"
+alias neofetch="neofetch --kitty --source ~/Pictures/corro.png --crop_mode fit --size 300px"
+
+# nnn options
+export NNN_OPTS="H"
+export NNN_FIFO="/tmp/nnn.fifo"
+export NNN_PLUG="p:preview-tui;m:xdgdefault"
+
+# nnn cd on quit
+n ()
+{
+    # Block nesting of nnn in subshells
+    if [[ "${NNNLVL:-0}" -ge 1 ]]; then
+        echo "nnn is already running"
+        return
+    fi
+
+    # The behaviour is set to cd on quit (nnn checks if NNN_TMPFILE is set)
+    # If NNN_TMPFILE is set to a custom path, it must be exported for nnn to
+    # see. To cd on quit only on ^G, remove the "export" and make sure not to
+    # use a custom path, i.e. set NNN_TMPFILE *exactly* as follows:
+    #     NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
+    export NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
+
+    # Unmask ^Q (, ^V etc.) (if required, see `stty -a`) to Quit nnn
+    # stty start undef
+    # stty stop undef
+    # stty lwrap undef
+    # stty lnext undef
+
+    # The backslash allows one to alias n to nnn if desired without making an
+    # infinitely recursive alias
+    \nnn "$@"
+
+    if [ -f "$NNN_TMPFILE" ]; then
+            . "$NNN_TMPFILE"
+            rm -f "$NNN_TMPFILE" > /dev/null
+    fi
+}
