9c0d5e05eaf6e807ec8284a98019244774674c08
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2023-09-05 15:03:47 -0700

    add latest config updates

--- .config/nvimuser/init.lua
+++ .config/nvimuser/init.lua
@@ -54,70 +54,13 @@ return {
       "folke/neoconf.nvim",
       event = "VimEnter",
     },
-    -- {
-    --   "dhruvasagar/vim-table-mode",
-    --   event = "BufRead",
-    -- },
     {
       "lambdalisue/suda.vim",
       lazy = false,
     },
     {
-      "zbirenbaum/copilot.lua",
-      cmd = "Copilot",
-      event = "InsertEnter",
-      config = function()
-        require("copilot").setup({
-          suggestion = { enabled = false },
-          panel = { enabled = false },
-        })
-      end
-    },
-    {
-      "zbirenbaum/copilot-cmp",
-      dependencies = { "copilot.lua" },
-      event = "InsertEnter",
-      config = function()
-        require("copilot_cmp").setup({})
-      end,
-    },
-    {
-      "onsails/lspkind.nvim",
-      opts = {
-        symbol_map = { Copilot = "" }
-      }
-    },
-    {
-      -- override nvim-cmp plugin
-      "hrsh7th/nvim-cmp",
-      dependencies = {
-        "zbirenbaum/copilot-cmp", -- add cmp source as dependency of cmp
-      },
-      -- override the options table that is used in the `require("cmp").setup()` call
-      opts = function(_, opts)
-        -- opts parameter is the default options table
-        -- the function is lazy loaded so cmp is able to be required
-        local cmp = require "cmp"
-        -- modify the sources part of the options table
-        opts.sources = cmp.config.sources {
-          { name = "nvim_lsp", priority = 1000 },
-          { name = "luasnip",  priority = 750 },
-          { name = "copilot",  priority = 625 },
-          { name = "buffer",   priority = 500 },
-          { name = "path",     priority = 250 },
-        }
-
-        -- return the new table to be used
-        return opts
-      end,
-    },
-    -- {
-    --   "chrisbra/csv.vim",
-    --   event = "BufEnter *.csv"
-    -- },
-    {
       "nvim-treesitter/nvim-treesitter",
-      dependencies = { "HiPhish/nvim-ts-rainbow2" },
+      -- dependencies = { "HiPhish/nvim-ts-rainbow2" },
       opts = function(_, opts)
         opts.rainbow = {
           enable = true,
