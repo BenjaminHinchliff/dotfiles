9f472908df1bf8aed4661d248c1fed4fe607d55c
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2024-08-26 12:31:23 -0700

    add new nvchad config

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,14 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cunsorline!
+
+vim.opt.relativenumber = true
+
+vim.filetype.add({
+    extension = {
+        pl = "prolog",
+    },
+})
