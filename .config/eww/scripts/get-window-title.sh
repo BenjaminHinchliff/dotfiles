9b02e17c454386cadc65c3972bbbbda7fdcf811e
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2024-08-27 16:23:23 -0700

    fix substr for active window

--- .config/eww/scripts/get-window-title.sh
+++ .config/eww/scripts/get-window-title.sh
@@ -2,4 +2,4 @@
 hyprctl activewindow -j | jq --raw-output .title
 socat -u UNIX-CONNECT:"$XDG_RUNTIME_DIR"/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock - | \
   stdbuf -o0 awk -F '>>|,' \
-  '/^activewindow>>/{if (length($3) > 90) {print substr($3, 1, 44) "..." substr($3, 11, 43)} else {print $3}}'
+  '/^activewindow>>/{if (length($3) > 90) {print substr($3, 1, 44) "..." substr($3, length($3) - 43)} else {print $3}}'
